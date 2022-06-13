f9d791b411ddad7fcd5b716fb9c56775482d7309
Frédéric Glorieux <frederic.glorieux@fictif.org>
2022-06-14 01:12:59 +0200

    OK, let’s try update on push

--- github.php
+++ github.php
@@ -1,32 +1,24 @@
 <?php
 
-$file = __DIR__ . '/github.log';
-// avoid log attack
-if (filesize($file) < 2000000) {
-    $out = fopen($file, 'a+');
-    fwrite($out, log_event() . "\n\n");
-    fclose($out);    
-}
+
+
 // get the sha 
+$sha = $SERVER['X-Github-Sha'];
+// check last sha
+$sha_file = '.htsha'; // should be invisible
+$sha_last = file_get_contents($sha_file);
+// bug or attack ?
+if ($sha == $sha_last) return;
+file_put_contents($sha_file, $sha);
 
+$log_file = __DIR__ . '/github.log';
+// 2Mb log ? Something is wrong
+if (filesize($log_file) > 2000000) return;
 
-
-function log_event() {
-    $request = '';
-    /*
-    $request = "$_SERVER[REQUEST_METHOD] $_SERVER[REQUEST_URI] $_SERVER[SERVER_PROTOCOL]\n";
-  
-
-    */
-    $request .= date(DATE_ATOM) . "\n";
-    $ip = $_SERVER['REMOTE_ADDR'];
-
-    $request .= "IP: $ip\n";
-    foreach (getallheaders() as $name => $value) {
-        $request .= "$name: $value\n";
-    }
-    $request .= file_get_contents('php://input');
-  
-    return $request;
-}
-
+$fopen = fopen($log_file, 'a+');
+fwrite($fopen, date(DATE_ATOM) . ' ' . $sha . "\n");
+// git pull
+$lines=array();
+exec("git pull 2>&1", $lines);
+fwrite($fopen, implode("\n", $lines));
+fclose($fopen);
