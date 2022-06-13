4933738c08540280293bcac1ef2e40934b36fbed
Frédéric Glorieux <frederic.glorieux@fictif.org>
2022-06-13 14:16:15 +0200

    push

--- github.php
+++ github.php
@@ -1,7 +1,19 @@
 <?php 
-$post = file_get_contents('php://input');
 $out = fopen(__DIR__ . '/github.log', 'a+');
-fwrite($out, $post . "\n\n");
+fwrite($out, get_raw_http_request() . "\n\n");
 fclose($out);
 echo "Thanks";
+
+function get_raw_http_request() {
+
+    $request = "$_SERVER[REQUEST_METHOD] $_SERVER[REQUEST_URI] $_SERVER[SERVER_PROTOCOL]\n";
+  
+    foreach (getallheaders() as $name => $value) {
+      $request .= "$name: $value\n";
+    }
+  
+    $request .= "\n" . file_get_contents('php://input');
+  
+    return $request;
+}
 
