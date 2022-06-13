cd3271d8b830e30a158e2f5d4066fafbe60c8747
Frédéric Glorieux <frederic.glorieux@fictif.org>
2022-06-13 23:48:12 +0200

    test sha as key

--- github.php
+++ github.php
@@ -1,18 +1,24 @@
 <?php 
 $out = fopen(__DIR__ . '/github.log', 'a+');
-fwrite($out, get_raw_http_request() . "\n\n");
+fwrite($out, request() . "\n\n");
 fclose($out);
 echo "Thanks";
 
-function get_raw_http_request() {
-
+function request() {
+    $request = '';
+    /*
     $request = "$_SERVER[REQUEST_METHOD] $_SERVER[REQUEST_URI] $_SERVER[SERVER_PROTOCOL]\n";
   
+
+    */
+    $request .= date(DATE_ATOM) . "\n";
+    $ip = $_SERVER['REMOTE_ADDR'];
+
+    $request .= "IP: $ip\n";
     foreach (getallheaders() as $name => $value) {
-      $request .= "$name: $value\n";
+        $request .= "$name: $value\n";
     }
-  
-    $request .= "\n" . file_get_contents('php://input');
+    $request .= file_get_contents('php://input');
   
     return $request;
 }
