38d640f9067f6f02c87ea05383b18cf97b1f1d64
Frédéric Glorieux <frederic.glorieux@fictif.org>
2022-06-15 12:39:33 +0200

    More generic link to local resource

--- template.php
+++ template.php
@@ -19,8 +19,8 @@ $cts = Web::par('kuhn', '');
         <meta charset="utf-8"/>
         <title><?= Route::title('Galenus Verbatim') ?></title>
         <link  href="https://cdnjs.cloudflare.com/ajax/libs/viewerjs/1.10.5/viewer.min.css" rel="stylesheet"/>
-        <link rel="stylesheet" href="<?= Route::app_href() ?>verbatim.css"/>
-        <link rel="stylesheet" href="<?= Route::home_href() ?>theme/galenus.css"/>
+        <link rel="stylesheet" href="<?= Route::res_href('../verbatim/verbatim.css') ?>"/>
+        <link rel="stylesheet" href="<?= Route::res_href('theme/galenus.css') ?>"/>
     </head>
     <body class="<?=$body_class?>">
 <div id="all">
@@ -31,7 +31,7 @@ $cts = Web::par('kuhn', '');
                 <div class="titlesub">Γαληνὸς κατὰ λέξιν</div>
             </div>
             <div class="moto"><?= I18n::_('galenus.moto') ?></div>
-            <img class="banner" src="<?= Route::home_href() ?>theme/galenus-verbatim.jpg" />
+            <img class="banner" src="<?= Route::res_href('theme/galenus-verbatim.jpg') ?>" />
         </div>
     </header>
     <div id="content">
@@ -65,21 +65,21 @@ $cts = Web::par('kuhn', '');
     </div>
     <footer id="footer">
         <nav id="logos">
-            <a href="https://www.iufrance.fr/" title="Institut universitaire de France"><img alt="Institut Universitaire de France" src="<?=  Route::home_href() ?>theme/logo_IUF.png"/></a>
+            <a href="https://www.iufrance.fr/" title="Institut universitaire de France"><img alt="Institut Universitaire de France" src="<?=  Route::res_href('theme/logo_IUF.png') ?>"/></a>
 
-            <a href="http://www.orient-mediterranee.com/spip.php?rubrique314" title="UMR 8167 Orient et Méditerranée"><img alt="UMR 8167 Orient et Méditerranée" src="<?=  Route::home_href() ?>theme/logo_UMR8167.png"/></a>
+            <a href="http://www.orient-mediterranee.com/spip.php?rubrique314" title="UMR 8167 Orient et Méditerranée"><img alt="UMR 8167 Orient et Méditerranée" src="<?=  Route::res_href('theme/logo_UMR8167.png') ?>"/></a>
 
-            <a href="https://lettres.sorbonne-universite.fr/faculte-des-lettres/ufr/lettres/grec/" title="Faculté des Lettres de Sorbonne Université"><img alt="Faculté des Lettres de Sorbonne Université" src="<?=  Route::home_href() ?>theme/logo_sorbonne-lettres.png"/></a>
+            <a href="https://lettres.sorbonne-universite.fr/faculte-des-lettres/ufr/lettres/grec/" title="Faculté des Lettres de Sorbonne Université"><img alt="Faculté des Lettres de Sorbonne Université" src="<?=  Route::res_href('theme/logo_sorbonne-lettres.png') ?>"/></a>
 
 
 
-            <a href="https://humanites-biomedicales.sorbonne-universite.fr/" title="Initiative humanités biomédicales de l’Alliance Sorbonne Université"><img alt="Initiative humanités biomédicales de l’Alliance Sorbonne Université" src="<?=  Route::home_href() ?>theme/logo_humabiomed.png"/></a>
+            <a href="https://humanites-biomedicales.sorbonne-universite.fr/" title="Initiative humanités biomédicales de l’Alliance Sorbonne Université"><img alt="Initiative humanités biomédicales de l’Alliance Sorbonne Université" src="<?=  Route::res_href('theme/logo_humabiomed.png') ?>"/></a>
 
-            <a href="#" onmouseover="this.href='ma'+'i'+'lto:'+'etymologika' + '\u0040gm' + 'ail.com';"><img style="opacity: 0.7;" src="<?=  Route::home_href() ?>theme/enveloppe.png"/></a>
+            <a href="#" onmouseover="this.href='ma'+'i'+'lto:'+'etymologika' + '\u0040gm' + 'ail.com';"><img style="opacity: 0.7;" src="<?=  Route::res_href('theme/enveloppe.png') ?>"/></a>
         </nav>
     </footer>
 </div>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/viewerjs/1.10.5/viewer.min.js"></script>
-        <script src="<?= Route::home_href() ?>theme/galenus.js"></script>
+        <script src="<?= Route::res_href('theme/galenus.js') ?>"></script>
     </body>
 </html>
