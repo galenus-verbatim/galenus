fe8798259a9cd0100572b25c0f49bc7a523c866a
Frédéric Glorieux <frederic.glorieux@fictif.org>
2022-06-18 15:27:55 +0200

    Zotero resources should be now updated

--- build.php
+++ build.php
@@ -8,7 +8,6 @@ use Oeuvres\Kit\{LoggerCli};
 $logger = new LoggerCli(LogLevel::DEBUG);
 Galenus::setLogger($logger);
 Verbatim::connect(Galenus::db_file());
-echo "Zot\n";
 Galenus::zotero();
 // generate sitemap.xml ??
 // require_once(dirname(__DIR__) . "/sitemap.php");
